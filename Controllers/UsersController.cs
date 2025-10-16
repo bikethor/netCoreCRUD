ee7b96651c6cbf0a50a55220b862e233289c5682
Victor Elizondo <avicto@hotmail.com>
2025-10-15 22:52:20 -0600

    CRUD WITH procedures

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -2,12 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NetCoreExercise.Data;
 using NetCoreExercise.Models;
+using System.Data;
+using Microsoft.Data.SqlClient;
 
 namespace NetCoreExercise.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-
     public class UsersController : ControllerBase
     {
         private readonly AppDbContext _context;
@@ -21,68 +22,90 @@ namespace NetCoreExercise.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            var users = await _context.Users
+                .FromSqlRaw("EXEC spi_get_users")
+                .ToListAsync();
+
+            return Ok(users);
         }
 
-        //GET: api/users/{id}
+        // GET: api/users/{id}
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUser(int id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var users = await _context.Users
+                .FromSqlRaw("EXEC spi_get_user_by_id @Id = {0}", id)
+                .AsNoTracking()
+                .ToListAsync();
+
+            var user = users.FirstOrDefault();
 
             if (user == null)
                 return NotFound();
 
-            return user;
+            return Ok(user);
         }
 
-        //POST: api/users
+        // POST: api/users
         [HttpPost]
         public async Task<ActionResult<User>> CreateUser(User user)
         {
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            var connection = _context.Database.GetDbConnection();
 
-            // Returns 201 Created with the new user info
+            await connection.OpenAsync();
+            using var command = connection.CreateCommand();
+            command.CommandText = "spi_insert_user";
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.Add(new SqlParameter("@Username", user.Username));
+            command.Parameters.Add(new SqlParameter("@FirstName", user.FirstName));
+            command.Parameters.Add(new SqlParameter("@LastName", user.LastName));
+            command.Parameters.Add(new SqlParameter("@operationDate", user.operationDate));
+
+            var result = await command.ExecuteScalarAsync();
+            user.Id = Convert.ToInt32(result);
+
             return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
         }
 
-        //PATCH: api/users/{id}
+        // PATCH: api/users/{id}
         [HttpPatch("{id}")]
         public async Task<IActionResult> UpdateUser(int id, [FromBody] User updatedUser)
         {
-            var existingUser = await _context.Users.FindAsync(id);
+            var connection = _context.Database.GetDbConnection();
 
-            if (existingUser == null)
-                return NotFound();
+            await connection.OpenAsync();
+            using var command = connection.CreateCommand();
+            command.CommandText = "spi_update_user";
+            command.CommandType = CommandType.StoredProcedure;
 
-            // Update fields only if they are different
-            existingUser.Username = updatedUser.Username;
-            existingUser.FirstName = updatedUser.FirstName;
-            existingUser.LastName = updatedUser.LastName;
-            existingUser.operationDate = updatedUser.operationDate;
+            command.Parameters.Add(new SqlParameter("@Id", id));
+            command.Parameters.Add(new SqlParameter("@Username", updatedUser.Username));
+            command.Parameters.Add(new SqlParameter("@FirstName", updatedUser.FirstName));
+            command.Parameters.Add(new SqlParameter("@LastName", updatedUser.LastName));
+            command.Parameters.Add(new SqlParameter("@operationDate", updatedUser.operationDate));
 
-            await _context.SaveChangesAsync();
+            await command.ExecuteNonQueryAsync();
 
-            return Ok(existingUser);
+            return Ok(updatedUser);
         }
 
-        //DELETE: api/users/{id}
+        // DELETE: api/users/{id}
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteUser(int id)
         {
-            var user = await _context.Users.FindAsync(id);
+            var connection = _context.Database.GetDbConnection();
 
-            if (user == null)
-                return NotFound();
+            await connection.OpenAsync();
+            using var command = connection.CreateCommand();
+            command.CommandText = "spi_delete_user";
+            command.CommandType = CommandType.StoredProcedure;
 
-            _context.Users.Remove(user);
-            await _context.SaveChangesAsync();
+            command.Parameters.Add(new SqlParameter("@Id", id));
+
+            await command.ExecuteNonQueryAsync();
 
             return NoContent();
         }
-        
     }
-
-
 }
